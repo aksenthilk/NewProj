fdb2cd8038dfba9ab5174066bd1fda736a13079f
aksenthilk <aksenthilk@gmail.com>
2018-06-27 16:14:22 +0530

    Made changes in the Runner Class

--- src/test/java/com/sen/runner/RunTest.java
+++ src/test/java/com/sen/runner/RunTest.java
@@ -9,8 +9,7 @@ import cucumber.api.junit.Cucumber;
 @CucumberOptions(features = ".\\src\\main\\java\\com\\sen\\features\\FreeCRM.feature",
                  glue = {".\\src\\main\\java\\com\\sen\\steps"},
                  dryRun = true,
-                 monochrome = true,
-                 format={"pretty","html:test-output","json:json-output/cucumber.json","junit:junit-output/cucumber.xml"}
+                 plugin={"pretty","html:test-output","json:json-output/cucumber.json","junit:junit-output/cucumber.xml"}
                 , strict = true		 
                 		 )
 //"cucumber.api.PendingException:TODO:implement me"  exception is thrown when strict=true and if any steps are unimplemented
@@ -18,3 +17,5 @@ import cucumber.api.junit.Cucumber;
 public class RunTest {
 
 }
+//removed monochrome = true,
+//removed format and added plugin
